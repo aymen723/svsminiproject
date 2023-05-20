4bf6e8682818e410a5ab5436ffdf694f336e6738
aymen723 <benmohammed.a.a@gmail.com>
2023-05-20 13:08:25 +0100

    now this branch is working to safe if things go bad

--- src/svsminiproject/Main.java
+++ src/svsminiproject/Main.java
@@ -5,48 +5,28 @@ import java.util.Set;
 
 public class Main {
 
-	// public static void main(String[] args) {
-	// // TODO Auto-generated method stub
-	// Place p1 = new Place(1, "P1");
-	// Place p2 = new Place(2, "P2");
-
-	// // Create transitions
-	// Transition t1 = new Transition(1, "T1");
-
-	// // Create arcs
-	// Arc arc1 = new Arc(p1, t1, 1, ArcDirection.IN);
-	// Arc arc2 = new Arc(p2, t1, 1, ArcDirection.OUT);
-
-	// // Create a PetriNet
-	// Petrinet petriNet = new Petrinet();
-	// petriNet.addPlace(p1);
-	// petriNet.addPlace(p2);
-	// petriNet.addTransition(t1);
-
-	// // Create an initial marking
-	// Marking initialMarking = new Marking();
-	// initialMarking.setToken(p1, 1);
-
-	// // Generate the marking graph
-	// Set<Marking> markingGraph = petriNet.generateMarkingGraph(initialMarking);
-
-	// // Print the marking graph
-	// for (Marking marking : markingGraph) {
-	// System.out.println("Marking: " + marking.getTokens());
-	// }
-	// }
 	public static void main(String[] args) {
 		// Create a Petri net and its graph of marking
 		Petrinet petriNet = new Petrinet();
 
-		Place p1 = new Place("p1", 1);
+		Place p0 = new Place("p0", 1);
+		Place p1 = new Place("p1", 2);
 		Place p2 = new Place("p2", 0);
+
+		petriNet.addPlace(p0);
 		petriNet.addPlace(p1);
 		petriNet.addPlace(p2);
+		Transition t0 = new Transition("t1");
 
+		t0.addInputPlace(p0);
+		t0.addOutputPlace(p0);
+		t0.addInputPlace(p1);
+		t0.addOutputPlace(p2);
 		Transition t1 = new Transition("t1");
 		t1.addInputPlace(p1);
-		t1.addOutputPlace(p2);
+		t1.addInputPlace(p2);
+
+		petriNet.addTransition(t0);
 		petriNet.addTransition(t1);
 
 		// Generate the NuSMV code
--- src/svsminiproject/Transition.java
+++ src/svsminiproject/Transition.java
@@ -3,44 +3,6 @@ package svsminiproject;
 import java.util.ArrayList;
 import java.util.List;
 
-// public class Transition {
-
-// private int id;
-// private String name;
-// private List<Arc> incomingArcs;
-// private List<Arc> outgoingArcs;
-
-// public Transition(int id, String name) {
-// this.id = id;
-// this.name = name;
-// incomingArcs = new ArrayList<>();
-// outgoingArcs = new ArrayList<>();
-// }
-
-// public int getId() {
-// return id;
-// }
-
-// public String getName() {
-// return name;
-// }
-
-// public List<Arc> getIncomingArcs() {
-// return incomingArcs;
-// }
-
-// public List<Arc> getOutgoingArcs() {
-// return outgoingArcs;
-// }
-
-// public void addIncomingArc(Arc arc) {
-// incomingArcs.add(arc);
-// }
-
-// public void addOutgoingArc(Arc arc) {
-// outgoingArcs.add(arc);
-// }
-// }
 class Transition {
     String name;
     List<Place> inputPlaces;
